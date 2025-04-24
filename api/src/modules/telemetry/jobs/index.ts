7e18aa843054f8eb88fecc7db8927d547106e2ee
Emerson <emersonlucenaoficial@gmail.com>
2025-04-24 11:08:44 -0300

    uncomment cronjob

--- api/src/modules/telemetry/jobs/index.ts
+++ api/src/modules/telemetry/jobs/index.ts
@@ -1,26 +1,19 @@
 import TelemetryRepository from "@/modules/telemetry/telemetry.repository";
-import { subHours, startOfHour } from "date-fns";
-import { getBrazilianUTCDate } from "../../shared/utils/index";
 
 export const runSaveKWhJob = async () => {
   console.log("🟡 Running hourly job to save kWh");
 
-  console.log(
-    "🟢🟢🟢🟢 subHours(getBrazilianUTCDate(), 1)",
-    startOfHour(subHours(getBrazilianUTCDate(), 1))
-  );
+  const powerFromLastHour = await TelemetryRepository.getPowerFromLastHour();
 
-  // const powerFromLastHour = await TelemetryRepository.getPowerFromLastHour();
+  const totalPowerFromLastHour = powerFromLastHour.reduce((acc, report) => {
+    return acc + report.kW;
+  }, 0);
 
-  // const totalPowerFromLastHour = powerFromLastHour.reduce((acc, report) => {
-  //   return acc + report.kW;
-  // }, 0);
+  const powerInKwh = totalPowerFromLastHour / 60;
 
-  // const powerInKwh = totalPowerFromLastHour / 60;
-
-  // await TelemetryRepository.saveKWhPerHour(powerInKwh);
-  // await TelemetryRepository.incrementKWhInCurrentMonth(powerInKwh);
-  // await TelemetryRepository.incrementKWhInCurrentDay(powerInKwh);
+  await TelemetryRepository.saveKWhPerHour(powerInKwh);
+  await TelemetryRepository.incrementKWhInCurrentMonth(powerInKwh);
+  await TelemetryRepository.incrementKWhInCurrentDay(powerInKwh);
 
   console.log("🟢 Hourly job ran successfully!");
 };
