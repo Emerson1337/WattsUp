13a556d6738e869845191e7d9881289440e5517a
Emerson <emersonlucenaoficial@gmail.com>
2025-04-24 21:25:10 -0300

    add react router project setup

---
+++ dashboard/app/dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { AreaChartInteractive } from '@/components/charts/AreaChartInteractive';
+
+export function Dashboard() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+      <AreaChartInteractive />
+    </main>
+  );
+}
+
