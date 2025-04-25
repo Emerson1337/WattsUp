4f78df4db9a8925da39b1f2e84971a11a46c4e5a
Emerson <emersonlucenaoficial@gmail.com>
2025-04-25 11:59:18 -0300

    remove brazilian utc date logic

--- api/src/modules/shared/utils/index.ts
+++ api/src/modules/shared/utils/index.ts
@@ -1,7 +1,3 @@
-const BRAZILIAN_TZ_OFFSET = -180;
-
 export function getBrazilianUTCDate() {
-  const now = new Date();
-  const offset = BRAZILIAN_TZ_OFFSET;
-  return new Date(now.getTime() + offset * 60 * 1000);
+  return new Date();
 }
