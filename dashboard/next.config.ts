9ce5d48a27c7f7bd7162bf3861376db06ee8797b
Emerson <emersonlucenaoficial@gmail.com>
2025-05-02 17:32:36 -0300

    fix build issues

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -1,24 +1,61 @@
 import type { NextConfig } from "next";
 
+interface Rule {
+  test?: { test?: (input: string) => boolean };
+}
+
 const nextConfig: NextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          as: "*.js",
+          loaders: ["@svgr/webpack"],
+        },
+      },
+    },
+  },
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
-      },
-      {
-        protocol: "http",
-        hostname: "**",
+        hostname: "a.storyblok.com",
+        port: "",
       },
     ],
   },
-  turbopack: {
-    rules: {
-      "*.svg": {
-        loaders: ["@svgr/webpack"],
-        as: "*.js",
-      },
+
+  // Keep this until Turbo handles SVGs in production builds
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule: Rule) =>
+      rule.test?.test?.(".svg")
+    );
+
+    if (fileLoaderRule) {
+      config.module.rules.push(
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/,
+        },
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [/url/] },
+          use: ["@svgr/webpack"],
+        }
+      );
+
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    return config;
+  },
+
+  logging: {
+    fetches: {
+      fullUrl: true,
     },
   },
 };
