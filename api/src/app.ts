062dd34a5781b173cfb474c3de7a9eeb9af8d729
Emerson <emersonlucenaoficial@gmail.com>
2025-05-13 23:19:51 -0300

    add swagger and api documentation

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import router from "@/routes";
 import "@/infra/crons/cronjob-runner";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+const swaggerDocument = YAML.load(
+  path.join(__dirname, ".", "swagger/openapi.yaml")
+);
+
+router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
