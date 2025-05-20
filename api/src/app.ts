467a6272434d28be6d89496fbcfc2b2916302d39
Emerson <emersonlucenaoficial@gmail.com>
2025-05-19 23:09:13 -0300

    load swagger only locally

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,18 +1,22 @@
 import express from "express";
 import router from "@/routes";
 import "@/infra/crons/cronjob-runner";
-import swaggerUi from "swagger-ui-express";
-import YAML from "yamljs";
-import path from "path";
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
-const swaggerDocument = YAML.load(
-  path.join(__dirname, ".", "swagger/openapi.yaml")
-);
 
-router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
+
+if (process.env.NODE_ENV === "development") {
+  const swaggerDocument = YAML.load(
+    path.join(__dirname, ".", "swagger/openapi.yaml")
+  );
+
+  router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
 
 export default app;
